82019b943b7622a06a5c0fcc6d6b6d0d11fee7c8
maferpalomarolvera00-spec <maferpalomarolvera00@gmail.com>
2026-01-17 11:41:22 -0600

    Primera Subida del archivo de ejemplo Python

---
+++ ejemplo.py
@@ -0,0 +1,15 @@
+#COMENTARIO:
+#Son una descripción del programa o código
+#los comentarios los omite o toma en cuenta python
+
+#EJEMPLO
+print("Mia Palomar")
+print("Sanchez")
+print("Hola")
+
+print()
+
+print("SUMA= ", 5+2) #suma
+print("RESTA= ", 5-2) #resta
+print("MULTIPLICACION= ", 5*2) #multiplicacion
+print("DIVISION= ", 5/2) #division
