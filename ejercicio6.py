f23be48e61656a866b3f3547985f4cca34d2cfe6
maferpalomarolvera00-spec <maferpalomarolvera00@gmail.com>
2026-01-30 22:33:13 -0600

    EJERCICIOS

---
+++ ejercicio6.py
@@ -0,0 +1,11 @@
+#Pedir un número y decir si es par o impar
+#ENTRADA
+numero = int(input("Ingrese el numero: "))
+
+#PROCESO
+if numero%2==0:
+    print("Es par") #SALIDA
+else:
+    print("Es impar") #SALIDA
+
+
---
+++ ejercicio7.py
@@ -0,0 +1,21 @@
+#Pedir el nivel del agua en metros de una cisterna
+#ENTRADA
+nivel = float(input("Ingresa el nivel del agua en metros: "))
+
+#PROCESO
+if nivel > 6: 
+    print("Desbordamiento de Agua en Cisterna") #SALIDA
+elif nivel == 6:
+    print("Apagar bomba") 
+elif nivel >4 and nivel < 6:
+    print("Desacelerar bomba") 
+elif nivel > 2 and nivel <=4:
+    print("Bomba trabajando") 
+elif nivel>0 and nivel<=2:
+    print("Acelerar bomba de agua") 
+elif nivel==0:
+    print("Encender bomba de agua") 
+elif nivel<0:
+    print("Fuga en cisterna")
+else: 
+    print("Valor inexistente") 
